7b7f73cacd91e395cf01dca544e61e9bc74dc53c
Verrellaksono <Verrellaksono8@gmail.com>
2026-07-29 01:22:00 +0700

    build priority mart and update mart

--- 2_DW_Mart_Build/build_dw_marts.sql
+++ 2_DW_Mart_Build/build_dw_marts.sql
@@ -1,6 +1,5 @@
 -- duckdb dw_marts.duckdb -c ".read build_dw_marts.sql"
 
-
 -- Step 1: DW - Crate star schema tables
 .read 01_Create_Tables_DW.sql
 
@@ -8,7 +7,13 @@
 .read 02_load_schema_dw.sql
 
 -- Step 3: Mart - Create flat mart
-.read 03_create_flat_mart.
+.read 03_create_flat_mart.sql
 
 -- Step 4: Mart - Create skills demand mart
 .read 04_create_skills_mart.sql
+
+-- Step 5: Mart - Create priority mart
+.read 05_create_priority_mart.sql
+
+-- Step 6: Mart - Update priority mart
+.read 06_update_priority_mart.sql
