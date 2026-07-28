341f97e0fe464bc4c4f5707e7cd5074892ce5b4a
Verrellaksono <Verrellaksono8@gmail.com>
2026-07-29 00:50:06 +0700

    build skills demand mart

--- 2_DW_Mart_Build/build_dw_marts.sql
+++ 2_DW_Mart_Build/build_dw_marts.sql
@@ -8,4 +8,7 @@
 .read 02_load_schema_dw.sql
 
 -- Step 3: Mart - Create flat mart
-.read 03_create_flat_mart.sql
+.read 03_create_flat_mart.
+
+-- Step 4: Mart - Create skills demand mart
+.read 04_create_skills_mart.sql
